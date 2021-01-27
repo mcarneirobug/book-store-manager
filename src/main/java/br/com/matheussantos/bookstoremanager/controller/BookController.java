a8861ab887de8561630d6c061b9809087d08d2b9
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-01-26 22:35:42 -0300

    feat: Added swagger to controller

--- src/main/java/br/com/matheussantos/bookstoremanager/controller/BookController.java
+++ src/main/java/br/com/matheussantos/bookstoremanager/controller/BookController.java
@@ -1,13 +1,22 @@
 package br.com.matheussantos.bookstoremanager.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/books")
+@Api("Book Store Manager API")
 public class BookController {
 
+    @ApiOperation(value = "Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return.")
+    })
     @GetMapping
     public String helloWorld() {
         return "Hello Bookstore Manager";
