321e3fefabd680bd1acc1d32bc49b00c81baa519
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-01-26 22:15:28 -0300

    feat: Created BookController with one method

---
+++ src/main/java/br/com/matheussantos/bookstoremanager/controller/BookController.java
@@ -0,0 +1,16 @@
+package br.com.matheussantos.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello Bookstore Manager";
+    }
+
+}
